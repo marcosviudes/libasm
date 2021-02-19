f6da9e112e4bce52324eb7bee20a8b261bb74132
Marcos Viudes <mviudes@student.42madrid.com>
2021-02-19 15:00:51 +0100

    update main

--- main.c
+++ main.c
@@ -6,32 +6,109 @@
 /*   By: mviudes <mviudes@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 15:14:04 by mviudes           #+#    #+#             */
-/*   Updated: 2021/01/04 14:56:50 by mviudes          ###   ########.fr       */
+/*   Updated: 2021/02/19 14:57:38 by mviudes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
+#include	"libasm.h"
 
-int		ft_strlen(char	const *str);
+void ft_bzero(char *buffer, int size)
+{
+	while (size--)
+		*buffer++ = 0;
+}
 
 void	print_strlen(char* string){
-	printf("\t'%s' = %d (%d)\n", string, ft_strlen(string), (int)strlen(string));
+	printf("\t'%s' = ", string);
+	printf("%d (%d)\n", ft_strlen(string), (int)strlen(string));
 }
+
+void	print_strcpy(char* dest, char *src){
+	char ret[1024];
+	char *_dest = strdup(dest);
+	char *_src = strdup(src);
+	
+	printf("\tcopy src: '%s', to dest: '%s'\n", src, dest);
+	strcpy(_dest, _src);
+	printf("\t\t%s     '%s'\n","strcpy ret:", _dest);
+	free(_dest);
+	free(_src);
+	_dest = strdup(dest);
+	_src = strdup(src);
+	ft_strcpy(_dest, _src);
+	printf("\t\t%s '%s'","ft_strcpy ret: ",_dest);
+	printf("\n\n");
+	free(_dest);
+	free(_src);
+}
+
+void	print_strcmp(char *string1, char *string2)
+{
+	int a;
+	int b;
+
+	printf("\ts1: '%s' s2: '%s'", string1, string2);
+	a = strcmp(string1, string2);
+	b = ft_strcmp(string1, string2);
+	printf(" ret: %i (%i)", a, b);
+	printf("\n");
+}
+
 int		main()
 {
-	char *string = "Hello World";
 
+	char *string = "Hello World";
+	char *empty = "";
+	char buff[150] = {0};
+	
 	printf("TEST FT_STRLEN\n");
-	print_strlen("Hello world");
-	print_strlen("");
+	print_strlen(string);
+	print_strlen(empty);
 	print_strlen("asdfghijklmnopqrstuvwxyz0123456789");
 	print_strlen("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et");
 	print_strlen("1");
+	printf("\tErrno: %d", errno);
 	printf("\n");
 
-	//check_strlen();
+	#ifndef DEBUG
+		getchar();
+		system("clear");
+	#endif
+	
+	printf("TEST FT_STRCPY\n");
+	print_strcpy(string, "pene");
+	print_strcpy("me cago en la puta", string);
+	print_strcpy(empty, string);
+	print_strcpy(string, empty);
+//	print_strcpy(buff, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et");
+
+	#ifndef DEBUG
+		getchar();
+		system("clear");
+	#endif
+
+	printf("TEST FT_STRCMP\n");
+	print_strcmp(string, empty);
+	print_strcmp(string, "asdfghijklmnopqrstuvwxyz0123456789");
+	print_strcmp(buff,string);
+	print_strcmp(string, "pene");
+
+	#ifndef DEBUG
+		getchar();
+		system("clear");
+	#endif
+
+	char *prueba;
+
+	prueba = ft_strdup("pene");
+	printf("%s", prueba);
+	prueba = ft_strdup(string);
+	printf("%s", prueba);
+
+	#ifndef DEBUG
+		getchar();
+		system("clear");
+	#endif
 	return (0);
 }
 
